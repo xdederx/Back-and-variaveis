458b469f2144d6d29e99828aead77594d3e83090
carlos <dederolin@gmail.com>
2023-07-12 19:48:30 -0300

    finalização

--- Imc.js
+++ Imc.js
@@ -1,5 +1,5 @@
 let Altura = 1.90, Peso = 98;
 
-let Resultado = Peso / Altura / Altura;
+let Resultado = Peso / Altura / Altura; // peço para realizarem um teste, essa formula é simplificada para chegar no mesmo resultado
 
 console.log(Resultado);
